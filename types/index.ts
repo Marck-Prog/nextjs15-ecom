9b0a568c602d4238fb4464e25c46d3cda645b624
Marck-Prog <marckfriencecoto@gmail.com>
2025-03-28 22:49:26 +0800

    Add product creation and update functionality; implement file upload handling and UI components

--- types/index.ts
+++ types/index.ts
@@ -1,19 +1,25 @@
 import {
+  CarouselSchema,
   CartSchema,
+  DeliveryDateSchema,
+  OrderInputSchema,
   OrderItemSchema,
+  PaymentMethodSchema,
   ProductInputSchema,
+  ReviewInputSchema,
+  SettingInputSchema,
   ShippingAddressSchema,
+  SiteCurrencySchema,
+  SiteLanguageSchema,
   UserInputSchema,
+  UserNameSchema,
   UserSignInSchema,
   UserSignUpSchema,
-  OrderInputSchema,
-  ReviewInputSchema,
-  UserNameSchema,
+  WebPageInputSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
 export type IReviewInput = z.infer<typeof ReviewInputSchema>
-
 export type IReviewDetails = IReviewInput & {
   _id: string
   createdAt: string
@@ -21,10 +27,11 @@ export type IReviewDetails = IReviewInput & {
     name: string
   }
 }
-
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
+  settings: ISettingInput[]
+  webPages: IWebPageInput[]
   users: IUserInput[]
   products: IProductInput[]
   reviews: {
@@ -44,9 +51,8 @@ export type Data = {
     isPublished: boolean
   }[]
 }
-
+// Order
 export type IOrderInput = z.infer<typeof OrderInputSchema>
-
 export type IOrderList = IOrderInput & {
   _id: string
   user: {
@@ -55,7 +61,6 @@ export type IOrderList = IOrderInput & {
   }
   createdAt: Date
 }
-
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
@@ -65,3 +70,17 @@ export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>
 export type IUserName = z.infer<typeof UserNameSchema>
+
+// webpage
+export type IWebPageInput = z.infer<typeof WebPageInputSchema>
+
+// setting
+export type ICarousel = z.infer<typeof CarouselSchema>
+export type ISettingInput = z.infer<typeof SettingInputSchema>
+export type ClientSetting = ISettingInput & {
+  currency: string
+}
+export type SiteLanguage = z.infer<typeof SiteLanguageSchema>
+export type SiteCurrency = z.infer<typeof SiteCurrencySchema>
+export type PaymentMethod = z.infer<typeof PaymentMethodSchema>
+export type DeliveryDate = z.infer<typeof DeliveryDateSchema>
